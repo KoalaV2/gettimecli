c993b03696029dbc400700e552a3771b39a69441
KoalaV2 <theo.johanson@gmail.com>
2021-02-04 15:51:13 +0100

    yes

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # gettimecli
 CLI to get the schedule for my school
+
+Props to https://bit.ly/TayIsAsleep for inspiration and help with code
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import requests
-#import urlib3.request
 import time
-#from bs4 import BeautifulSoup
 import json
+import argparse
+import datetime
 def main():
     headers = {
     "Connection": "keep-alive",
@@ -17,7 +17,17 @@ def main():
     "Cookie": "ASP.NET_SessionId=5hgt3njwnabrqso3cujrrj2p"
     }
     idurl='https://web.skola24.se/api/encrypt/signature'
-    payload = { "signature":"20_el_a" }
+
+    parser = argparse.ArgumentParser(description="Prints out the schedule for skola24.se NTI Södertörn")
+    parser.add_argument('-c','--classid',type=str,help='Select the current class ID')
+    parser.add_argument('-w','--weekday',type=int,help='The number of the day, for example monday=1, tuesday=2')
+    args = parser.parse_args()
+    classid = args.classid
+    weekday = args.weekday
+    now = datetime.datetime.now()
+    weeknumber = datetime.date(now.year,now.month,now.day).isocalendar()[1]
+
+    payload = { "signature":classid }
     response = requests.post(idurl, data=json.dumps(payload), headers=headers)
     response2 = str(response.text)
     response3 = response2.split('"signature": "')[1].split('"')[0]
@@ -47,16 +57,33 @@ def main():
     responsesecond2 = (str(responsesecond.text))
     responsesecond3 = responsesecond2.split('"key": "')[1].split('"')[0]
 
-    payload3 = {"renderKey":responsesecond3,"host":"it-gymnasiet.skola24.se","unitGuid":"ZTEyNTdlZjItZDc3OC1mZWJkLThiYmEtOGYyZDA4NGU1YjI2","startDate":"null","endDate":"null","scheduleDay":0,"blackAndWhite":"false","width":758,"height":648,"selectionType":4,"selection":response3,"showHeader":"false","periodText":"","week":5,"year":2021,"privateFreeTextMode":"false","privateSelectionMode":"null","customerKey":""}
+    payload3 = {
+            "renderKey":responsesecond3,
+            "host":"it-gymnasiet.skola24.se",
+            "unitGuid":"ZTEyNTdlZjItZDc3OC1mZWJkLThiYmEtOGYyZDA4NGU1YjI2",
+            "startDate":"null",
+            "endDate":"null",
+            "scheduleDay":weekday,
+            "blackAndWhite":"false",
+            "width":758,
+            "height":648,
+            "selectionType":4,
+            "selection":response3,
+            "showHeader":"false",
+            "periodText":"",
+            "week":weeknumber,
+            "year":2021,
+            "privateFreeTextMode":"false",
+            "privateSelectionMode":"null",
+            "customerKey":""
+            }
     thirdurl='https://web.skola24.se/api/render/timetable'
     responsethird = requests.post(thirdurl,data=json.dumps(payload3),headers=headers2)
 
     result = json.loads(responsethird.text)
-    print(responsethird.text)
 
     for x in result['data']['lessonInfo']:
-        #print(f"Lektion namn: {x['texts'][0]}, börjar kl {x['timeStart']} och slutar kl {x['timeEnd']}")
-        if {x['dayOfWeekNumber'] == 1}:
-            print(f"Lektion namn: {x['texts'][0]}")
+        print(f"{x['texts'][0]}, börjar kl {x['timeStart']} och slutar kl {x['timeEnd']}")
+
 if __name__=="__main__":
     main()
