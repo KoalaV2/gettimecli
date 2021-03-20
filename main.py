bf1d90e68b55d30069b25be153616ed19193ad98
KoalaV2 <theo.johanson@gmail.com>
2021-03-20 15:40:40 +0100

    stuff

--- main.py
+++ main.py
@@ -5,8 +5,18 @@ import json
 import argparse
 import datetime
 
+def currentweekday():
+    import datetime as d
+    weekday = d.datetime.today().isoweekday()
+    if 1 <= weekday <= 5:
+        return weekday
+    return 0
 
-def firstrequest():
+
+def getData(weekday):
+    now = datetime.datetime.now()
+    weeknumber = datetime.date(now.year, now.month, now.day).isocalendar()[1]
+
     parser = argparse.ArgumentParser(description="Prints out the schedule for skola24.se NTI Södertörn")
     parser.add_argument('-c', '--classid', type=str, help='Select the current class ID')
     args = parser.parse_args()
@@ -28,10 +38,7 @@ def firstrequest():
     response = requests.post(idurl, data=json.dumps(payload), headers=headers)
     response2 = str(response.text)
     response3 = response2.split('"signature": "')[1].split('"')[0]
-    return (response3)
 
-
-def secondrequest():
     headers2 = {
         "Host": "web.skola24.se",
         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:85.0) Gecko/20100101 Firefox/85.0",
@@ -54,41 +61,7 @@ def secondrequest():
     responsesecond = requests.post(secondurl, data=payload2, headers=headers2)
     responsesecond2 = (str(responsesecond.text))
     responsesecond3 = responsesecond2.split('"key": "')[1].split('"')[0]
-    return (responsesecond3)
-
-
-def currentweekday():
-    import datetime as d
-    weekday = d.datetime.today().isoweekday()
-    if 1 <= weekday <= 5:
-        return weekday
-    return 0
-
-
-def main():
-    response3 = firstrequest()
-    weekday = currentweekday()
-    responsesecond3 = secondrequest()
-    now = datetime.datetime.now()
-    weeknumber = datetime.date(now.year, now.month, now.day).isocalendar()[1]
-    headers3 = {
-        "Host": "web.skola24.se",
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:85.0) Gecko/20100101 Firefox/85.0",
-        "Accept": "application/json, text/javascript, */*; q=0.01",
-        "Accept-Language": "en-US,en;q=0.5",
-        "Accept-Encoding": "gzip, deflate",
-        "Content-Type": "application/json",
-        "X-Scope": "8a22163c-8662-4535-9050-bc5e1923df48",
-        "X-Requested-With": "XMLHttpRequest",
-        "Content-Length": "4",
-        "Origin": "https://web.skola24.se",
-        "Connection": "close",
-        "Referer": "https://web.skola24.se/timetable/timetable-viewer/it-gymnasiet.skola24.se/IT-Gymnasiet%20S%C3%B6dert%C3%B6rn/",
-        "Cookie": "ASP.NET_SessionId=5hgt3njwnabrqso3cujrrj2p",
-        "Sec-GPC": "1",
-        "DNT": "1"
-    }
-
+    headers3 = headers2
     payload3 = {
         "renderKey": responsesecond3,
         "host": "it-gymnasiet.skola24.se",
@@ -111,6 +84,10 @@ def main():
     }
     thirdurl = 'https://web.skola24.se/api/render/timetable'
     responsethird = requests.post(thirdurl, data=json.dumps(payload3), headers=headers3)
+    return responsethird
+
+def main():
+    responsethird = getData(currentweekday())
     result = json.loads(responsethird.text)
     a = []
 
